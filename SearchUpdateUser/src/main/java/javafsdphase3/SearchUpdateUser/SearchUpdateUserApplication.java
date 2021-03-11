045c2ae684563323d52298848ddae88330e260ce
Luc <ludan1214@gmail.com>
2021-03-11 15:47:19 -0600

    Project Init

---
+++ SearchUpdateUser/src/main/java/javafsdphase3/SearchUpdateUser/SearchUpdateUserApplication.java
@@ -0,0 +1,13 @@
+package javafsdphase3.SearchUpdateUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchUpdateUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchUpdateUserApplication.class, args);
+	}
+
+}
---
+++ SearchUpdateUser/src/test/java/javafsdphase3/SearchUpdateUser/SearchUpdateUserApplicationTests.java
@@ -0,0 +1,13 @@
+package javafsdphase3.SearchUpdateUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchUpdateUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
