5131481915646319f6ba2001ed59f7b857e09772
Luc <ludan1214@gmail.com>
2021-03-16 10:53:29 -0500

    Initial

---
+++ DisplayingUserFeedback/src/test/java/com/javafsdphase3/DisplayingUserFeedback/DisplayingUserFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javafsdphase3.DisplayingUserFeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisplayingUserFeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
