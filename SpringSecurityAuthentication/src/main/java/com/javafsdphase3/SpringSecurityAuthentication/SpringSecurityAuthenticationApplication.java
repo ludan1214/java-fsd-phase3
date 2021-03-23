bfaf74630401f2c2beb1246ffb300a5668d2292d
Luc <ludan1214@gmail.com>
2021-03-23 11:12:31 -0500

    Project 4 implemented

---
+++ SpringSecurityAuthentication/src/main/java/com/javafsdphase3/SpringSecurityAuthentication/SpringSecurityAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.javafsdphase3.SpringSecurityAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthenticationApplication.class, args);
+	}
+
+}
